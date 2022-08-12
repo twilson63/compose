277aafbb005cd833a572cc493287ac32435b6cd3
Tom Wilson <tom@hyper.io>
2022-08-12 21:56:59 +0000

    first commit

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,14 +1,32 @@
-<nav
-  class="flex space-x-4 h-[75px] bg-secondary text-secondary-content flex items-center"
->
-  <a class="btn btn-ghost" href="/">Home</a>
-  <a class="btn btn-ghost" href="/about">About</a>
-</nav>
+<script>
+  import Howitworks from "./home/how-it-works.svelte";
+</script>
+
 <main>
-  <section class="hero min-h-screen bg-base-100">
-    <div class="hero-content flex-col">
-      <h1 class="text-8xl text-primary-content font-bold">⚡️HypAR⚡️</h1>
-      <p>The Ultimate Permaweb Stack!</p>
+  <section class="hero min-h-[600px] bg-base-200">
+    <div class="hero-content space-x-16">
+      <div class="max-w-md flex-1">
+        <h1 class="text-5xl font-bold">
+          Welcome to<br />PermaPages!
+          <span class="badge badge-secondary">v1</span>
+        </h1>
+        <p class="mt-8 mb-16">
+          Create and manage your own permanent web3 profile and permaweb pages
+          built on Arweave.
+        </p>
+        <div class="flex space-x-2">
+          <button class="btn btn-primary">Connect Wallet</button>
+          <button class="btn btn-outline">Learn more</button>
+        </div>
+      </div>
+      <div class="hidden md:inline-block flex-1 place-items-center">
+        <img
+          class="w-3/4"
+          src="assets/permapageslanding-p-800.png"
+          alt="permapages"
+        />
+      </div>
     </div>
   </section>
+  <Howitworks />
 </main>
